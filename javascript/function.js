7e409569c9a0ff466ee7adaa23baacb1a35c1fc0
Geethanjali <geethanjali641581@gmail.com>
2025-09-23 14:40:57 +0530

    ind3ex

---
+++ javascript/function.js
@@ -0,0 +1,35 @@
+//function
+//code resuability
+function add(){
+    var a=57;
+    var b=78;
+    // document.write(a+b);
+    console.log(a+b);
+    
+
+}
+add();
+
+//way 2 of using functions(Es6)
+const sub=()=>{
+    // function sub(){
+    var a=57;
+    var b=78;
+    // document.write(a+b);
+    console.log(a-b);
+}
+// }
+sub();
+
+//Es7
+const mul=(a,b)=>console.log(a*b);
+mul(2,4);
+
+
+let name="sruthi"
+	function demo(){
+	    let name="sru"
+	    document.write('hello my name is' $(name));
+	}
+document.write("hello,my name is" $(name));
+	demo();
